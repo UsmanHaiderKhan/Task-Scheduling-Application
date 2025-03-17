8b2972202f0fbb0a5be79d31a3238dd998fdc92a
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-03-17 23:54:00 +0100

    ♻️ Update : add project form and prepare project data

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,16 +1,32 @@
+import { useRef } from "react";
 import Input from "./Input";
 
-export default function NewProject() {
+export default function NewProject({ onAddProject }) {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+    function handleSave() {
+        const enteredTitle = title.current.value;
+        const enteredDescription = description.current.value;
+        const enteredDueDate = dueDate.current.value;
+
+        onAddProject({
+            title: enteredTitle,
+            description: enteredDescription,
+            dueDate: enteredDueDate,
+        });
+    }
     return (
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center gap-4 my-4 justify-end">
                 <li><button className="text-stone-800 hover:text-stone-850">Cancel</button></li>
-                <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+                <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
             </menu>
             <div>
-                <Input label="Project Title" />
-                <Input label="Description" textarea />
-                <Input label="Due Date" />
+                <Input ref={title} label="Project Title" />
+                <Input ref={description} label="Description" textarea />
+                <Input ref={dueDate} label="Due Date" />
 
             </div>
         </div>
