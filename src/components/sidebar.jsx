e3afb574e0b09acb613a7f486e7478ccb26b2dd7
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-03-17 23:53:06 +0100

    ♻️Update : add click function on add new project buttons

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,13 +1,13 @@
 import Button from "./Button.jsx";
 
-export default function Sidebar() {
+export default function Sidebar({ onStartAddProject }) {
     return (
         <aside className="sidebar w-1/3 md:w-72 text-stone-50 bg-stone-900 rounded-r-lg px-8 py-16">
             <div>
                 <h2 className="text-stone-200 mb-8 font-bold uppercase md:xl">Your Projects</h2>
             </div>
             <div>
-                <Button>
+                <Button onClick={onStartAddProject}>
                     + Add New Project
                 </Button>
             </div>
