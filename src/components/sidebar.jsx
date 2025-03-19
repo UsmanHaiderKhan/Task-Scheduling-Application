b5f5967e82ea973cb12fc6bd68a1f7d4c55a89cb
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-03-19 21:50:29 +0100

    ♻️Update : rearrange the code

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button.jsx";
 
-export default function Sidebar({ onStartAddProject }) {
+export default function Sidebar({ onStartAddProject, projects }) {
     return (
         <aside className="sidebar w-1/3 md:w-72 text-stone-50 bg-stone-900 rounded-r-lg px-8 py-16">
             <div>
@@ -10,6 +10,15 @@ export default function Sidebar({ onStartAddProject }) {
                 <Button onClick={onStartAddProject}>
                     + Add New Project
                 </Button>
+            </div>
+            <div>
+                <ul className="mt-8">
+                    {projects.map((project) => (
+                        <li key={project.id} >
+                            <button className="w-full text-left text-stone-400 py-1 px-2 my-1 rounded-sm hover:text-stone-200 hover:bg-stone-800">{project.title}</button>
+                        </li>
+                    ))}
+                </ul>
             </div>
         </aside>
     );
