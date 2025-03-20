226339c28c41dabbf129dd5b41a7547cee291e1b
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-03-20 22:18:36 +0100

    🎉✨ Add : New component to show the details of the selected project

---
+++ src/components/SelectedProject.jsx
@@ -0,0 +1,21 @@
+export default function SelectedProject({ project }) {
+    const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
+        day: "numeric",
+        month: "long",
+        year: "numeric"
+    });
+
+    return (
+        <div className="w-[35rem] mt-16">
+            <header className="pb-4 mb-4 border-b-2 border-stone-300">
+                <div className="flex justify-between items-center">
+                    <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
+                    <button className="text-stone-600 hover:text-stone-900">Delete</button>
+                </div>
+
+            </header>
+            <p className="text-stone-600 mb-4">{project.description}</p>
+            <p className="text-stone-600 whitespace-pre-wrap">{formattedDate}</p>
+        </div>
+    );
+}
