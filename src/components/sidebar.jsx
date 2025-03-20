556a23cbb31238c6450280b1a6511cd4e433c4c2
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-03-20 22:19:14 +0100

    ♻️ Update : add select project function to get project by id

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button.jsx";
 
-export default function Sidebar({ onStartAddProject, projects }) {
+export default function Sidebar({ onStartAddProject, projects, onSelectProject, projectId }) {
     return (
         <aside className="sidebar w-1/3 md:w-72 text-stone-50 bg-stone-900 rounded-r-lg px-8 py-16">
             <div>
@@ -13,11 +13,19 @@ export default function Sidebar({ onStartAddProject, projects }) {
             </div>
             <div>
                 <ul className="mt-8">
-                    {projects.map((project) => (
-                        <li key={project.id} >
-                            <button className="w-full text-left text-stone-400 py-1 px-2 my-1 rounded-sm hover:text-stone-200 hover:bg-stone-800">{project.title}</button>
-                        </li>
-                    ))}
+                    {projects.map((project) => {
+                        let cssClasses = "w-full text-left py-1 px-2 my-1 rounded-sm hover:text-stone-200 hover:bg-stone-800";
+                        if (project.id === projectId) {
+                            cssClasses += ' bg-stone-800';
+                        } else {
+                            cssClasses += ' text-stone-400';
+                        }
+                        return (
+                            <li key={project.id} >
+                                <button onClick={() => onSelectProject(project.id)} className={cssClasses}>{project.title}</button>
+                            </li>
+                        )
+                    })}
                 </ul>
             </div>
         </aside>
