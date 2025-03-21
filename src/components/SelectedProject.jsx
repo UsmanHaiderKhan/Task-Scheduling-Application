4740b57c29003796d8bba499fcbf554c4b79e852
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-03-21 23:01:03 +0100

    ✨♻️ Update : Add new components at their respective components

--- src/components/SelectedProject.jsx
+++ src/components/SelectedProject.jsx
@@ -1,4 +1,6 @@
-export default function SelectedProject({ project }) {
+import Tasks from "./Tasks.jsx";
+
+export default function SelectedProject({ project, onDelete }) {
     const formattedDate = new Date(project.dueDate).toLocaleDateString("en-US", {
         day: "numeric",
         month: "long",
@@ -10,12 +12,12 @@ export default function SelectedProject({ project }) {
             <header className="pb-4 mb-4 border-b-2 border-stone-300">
                 <div className="flex justify-between items-center">
                     <h1 className="text-3xl font-bold text-stone-600 mb-2">{project.title}</h1>
-                    <button className="text-stone-600 hover:text-stone-900">Delete</button>
+                    <button onClick={onDelete} className="text-stone-600 hover:text-stone-900">Delete</button>
                 </div>
-
+                <p className="text-stone-600 mb-4">{project.description}</p>
+                <p className="text-stone-600 whitespace-pre-wrap">{formattedDate}</p>
             </header>
-            <p className="text-stone-600 mb-4">{project.description}</p>
-            <p className="text-stone-600 whitespace-pre-wrap">{formattedDate}</p>
+            <Tasks />
         </div>
     );
 }
