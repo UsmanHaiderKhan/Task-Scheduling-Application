9c727bc3fdd76d17a32c9ea6ce89514237d88c42
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2025-03-22 23:37:12 +0100

    ♻️ update : show tasks with respective projects

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,14 +1,19 @@
 import NewTasks from "./NewTasks.jsx";
 
-export default function Tasks() {
+export default function Tasks({ tasks, onAdd, onDelete }) {
     return (
         <section>
             <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
-            <NewTasks />
-            <p className="text-stone-800 my-4">This project dont have any tasks yet.</p>
-            <ul>
-
-            </ul>
+            <NewTasks onAdd={onAdd} />
+            {tasks.length === 0 && (<p className="text-stone-800 my-4">This project don't have any tasks yet.</p>)}
+            {tasks.length > 0 && (<ul>
+                {tasks.map((task) => (
+                    <li key={task.id} className="flex items-center justify-between py-2 rounded-sm border-b-2 border-stone-300">
+                        <span className="text-stone-700">{task.text}</span>
+                        <button onClick={() => onDelete(task.id)} className="text-stone-700 hover:text-red-500">Clear</button>
+                    </li>
+                ))}
+            </ul>)}
         </section>
     );
 }
